2f501c3d172b14e5ba167ad0ab3aab6cb7a78277
Vince van Doorn <323888>
2020-04-24 00:03:18 +0200

    init

---
+++ home.php
@@ -0,0 +1,8 @@
+<header class="mainheader text-center text-white" >
+    <div class="headercontent">
+        <div class="container">
+            <h1 class="headertitle">Frikandelbroodje</h1>
+            <h2 class="headerondertitle">Beter dan broodje frikandel</h2>
+            <a class="btn btn-primary homebutton rounded-pill mt-5" role="button" href="#" style="background-image: url(&quot;assets/img/frikandelbroodjelogo2.png&quot;);background-color: rgba(238,105,9,0.92);">Word lid</a></div>
+    </div>
+</header>
---
+++ index.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/main.css">
+
+
+    <title>Hello, world!</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-dark navbar-expand-lg fixed-top navbar-custom">
+        <div class="container"><a class="navbar-brand" href="#">Frikandelbroodjes</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navbarResponsive"><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="nav navbar-nav ml-auto">
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="#">Sign Up</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="#">Log In</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+
+<?php include("./assets/footer.html");?>
+
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
+
+</html>
